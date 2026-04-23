b5d072605394a96745d049d95733b4c77a9ceca2
Yinghan Ma <mayinghan070110@gmail.com>
2026-04-23 23:21:37 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
     - GitHub: Michaelmayinghan
     - Wechat: MMyh0110
   
-    - 身份：UCL (伦敦大学学院) 机器人与人工智能本硕连读生,现在大一,2007年出生，性别男。
+    - 身份：UCL (伦敦大学学院) 机器人与人工智能本硕连读生,现在大一,2007年一月出生，性别男。
     - 技能：深耕 AI、机器学习、机器人控制理论和全栈开发。
     - 爱好：专业拳击摄影师，常驻伦敦。风格为“直视荒诞”。
     【工程项目】
